b73ca2d77440dbec77399f3970ef737ca342405f
Asiz96 <94669884+Asiz96@users.noreply.github.com>
2022-11-10 19:28:21 +0530

    Add Splash Screen
    Add Splash Screen

---
+++ cafe_system/cafe_system/splashScreen.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace cafe_system
+{
+    partial class splashScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(splashScreen));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.BackColor = System.Drawing.Color.Transparent;
+            this.lblMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMsg.ForeColor = System.Drawing.Color.White;
+            this.lblMsg.Location = new System.Drawing.Point(25, 311);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(0, 16);
+            this.lblMsg.TabIndex = 0;
+            // 
+            // splashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(559, 350);
+            this.Controls.Add(this.lblMsg);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "splashScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "splashScreen";
+            this.Load += new System.EventHandler(this.splashScreen_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblMsg;
+    }
+}
---
+++ cafe_system/cafe_system/splashScreen.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cafe_system
+{
+    public partial class splashScreen : Form
+    {
+        public splashScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void splashScreen_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        int step = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            switch (step)
+            {
+                case 0:
+                    //Enabled = false;
+                    timer1.Interval = 2000;
+                    break;
+
+                case 1:
+                    lblMsg.Text = "Database loading";
+                    lblMsg.ForeColor = Color.White;
+                    break;
+
+                case 2:
+                    lblMsg.Text = "Accounts loading";
+                    break;
+
+                case 3:
+                    lblMsg.Text = "System starting...100%";
+                    break;
+
+                case 4:
+                    this.Close();
+                    break;
+            }
+            step++;
+        }
+
+        
+    }
+}
